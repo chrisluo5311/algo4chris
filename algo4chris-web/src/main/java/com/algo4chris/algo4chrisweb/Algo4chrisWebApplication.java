7ef6085108e1603ef1028ee447a130b90f24bf86
Chris Luo <chrislo5311@gmail.com>
2022-03-31 17:22:43 +0800

    Add algo4chris modules

---
+++ algo4chris-web/src/main/java/com/algo4chris/algo4chrisweb/Algo4chrisWebApplication.java
@@ -0,0 +1,13 @@
+package com.algo4chris.algo4chrisweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algo4chrisWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Algo4chrisWebApplication.class, args);
+    }
+
+}
